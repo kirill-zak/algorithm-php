5aecde6b55b2b914478145db8e444e7ebb952536
kirill-zak <kirill.zak@gmail.com>
2022-10-19 10:54:31 +0300

    1-add-binary-search-algorithm Add binary search realization

---
+++ src/Algorithm/BinarySearch/BinarySearch.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KirillZak\Algorithm\Algorithm\BinarySearch;
+
+final class BinarySearch implements BinarySearchInterface
+{
+    /**
+     * @var list<mixed>
+     */
+    private array $itemList;
+
+    private int $low = 0;
+
+    private int $high;
+
+    private int $iterationCount = 0;
+
+    /**
+     * @param list<mixed> $itemList
+     */
+    public function __construct(array $itemList)
+    {
+        $this->itemList = $itemList;
+        $this->high = count($itemList);
+    }
+
+    /**
+     * @param mixed $searchedItem
+     * @return int|null
+     */
+    public function search(mixed $searchedItem): ?int
+    {
+        if (count($this->itemList) === 0) {
+            return null;
+        }
+
+        $this->iterationCount = 1;
+
+        while ($this->low <= $this->high) {
+            $this->iterationCount++;
+
+            $iterationResult = $this->performIteration($searchedItem);
+            if ($iterationResult !== null) {
+                return $iterationResult;
+            }
+        }
+
+        return null;
+    }
+
+    public function getIterationCount(): int
+    {
+        return $this->iterationCount;
+    }
+
+    private function performIteration(mixed $searchedItem): ?int
+    {
+        $middle = (int) ceil(($this->low + $this->high) / 2);
+        $middleValue = $this->itemList[$middle];
+
+        if ($middleValue === $searchedItem) {
+            return $middle;
+        }
+
+        if ($middleValue > $searchedItem) {
+            $this->high = $middle - 1;
+        } else {
+            $this->low = $middle + 1;
+        }
+
+        return null;
+    }
+}
---
+++ tests/Algorithm/BinarySearch/BinarySearchTest.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace KirillZak\Algorithm\Tests\Algorithm\BinarySearch;
+
+use Exception;
+use KirillZak\Algorithm\Algorithm\BinarySearch\BinarySearch;
+use KirillZak\Algorithm\Algorithm\BinarySearch\BinarySearchInterface;
+use PHPUnit\Framework\TestCase;
+
+final class BinarySearchTest extends TestCase
+{
+    private const ITEM_LIST_SIZE = 100;
+    private const ITEM_LIST_SIZE_FOR_INTERACTION_COUNT = 100;
+    private const ITERATION_COUNT_FOR_EMPTY_LIST = 0;
+    private const ITERATION_COUNT_FOR_LIST_WITH_100_ELEMENTS = 7;
+    private const SEARCHED_ITEM = 68498;
+    private const SEARCHED_ITEM_FOR_INTERACTION_COUNT = 57;
+    private const SEARCHED_ITEM_POSITION = 74;
+
+    /**
+     * @dataProvider searchDataProvider
+     *
+     * @param list<int> $itemList
+     * @param int|null $expectedResult
+     * @return void
+     */
+    public function testSearch(array $itemList, ?int $expectedResult): void
+    {
+        $binarySearch = $this->createBinarySearch($itemList);
+
+        $result = $binarySearch->search(self::SEARCHED_ITEM);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * @dataProvider getIterationCountDataProvider
+     *
+     * @param list<int> $itemList
+     * @param int $expectedResult
+     * @return void
+     */
+    public function testGetIterationCount(array $itemList, int $expectedResult): void
+    {
+        $binarySearch = $this->createBinarySearch($itemList);
+
+        $binarySearch->search(self::SEARCHED_ITEM_FOR_INTERACTION_COUNT);
+
+        $result = $binarySearch->getIterationCount();
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * @return array{string: array{string: list<int>, string: ?int}}
+     * @throws Exception
+     */
+    public function searchDataProvider(): array
+    {
+        return
+            [
+                'Empty item list' =>
+                    [
+                        'itemList' => [],
+                        'expectedResult' => null,
+                    ],
+                'Item found' =>
+                    [
+                        'itemList' => $this->createItemListWithSearchedItem(),
+                        'expectedResult' => self::SEARCHED_ITEM_POSITION,
+                    ],
+                'Item not found' =>
+                    [
+                        'itemList' => $this->createItemListWithoutSearchedItem(),
+                        'expectedResult' => null,
+                    ],
+            ];
+    }
+
+    /**
+     * @return array{string: array{string: list<int>, string: int}}
+     */
+    public function getIterationCountDataProvider(): array
+    {
+        return
+            [
+                'Empty item list' =>
+                    [
+                        'itemList' => [],
+                        'expectedResult' => self::ITERATION_COUNT_FOR_EMPTY_LIST,
+                    ],
+                'Item list with 100 elements' =>
+                    [
+                        'itemList' => $this->createItemListForGetInteractionCount(),
+                        'expectedResult' => self::ITERATION_COUNT_FOR_LIST_WITH_100_ELEMENTS,
+                    ],
+            ];
+    }
+
+    private function createBinarySearch(array $itemList): BinarySearchInterface
+    {
+        return new BinarySearch($itemList);
+    }
+
+    /**
+     * @return list<int>
+     * @throws Exception
+     */
+    private function createItemListWithSearchedItem(): array
+    {
+        return
+            array_merge(
+                $this->fillItemListBeforeSearchedValues(),
+                [self::SEARCHED_ITEM_POSITION => self::SEARCHED_ITEM],
+                $this->fillItemListAfterSearchedValue(),
+            );
+    }
+
+    /**
+     * @return list<int>
+     * @throws Exception
+     */
+    private function fillItemListBeforeSearchedValues(): array
+    {
+        $itemList = [];
+
+        while (count($itemList) < self::SEARCHED_ITEM_POSITION) {
+            $item = random_int(0, self::SEARCHED_ITEM - 1);
+
+            if (!in_array($item, $itemList, true)) {
+                $itemList[] = $item;
+            }
+        }
+
+        sort($itemList);
+
+        return array_values($itemList);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function fillItemListAfterSearchedValue(): array
+    {
+        $itemList = [];
+
+        while (count($itemList) < (self::ITEM_LIST_SIZE - self::SEARCHED_ITEM_POSITION)) {
+            $item = random_int(self::SEARCHED_ITEM + 1, 10000000000);
+
+            if (!in_array($item, $itemList, true)) {
+                $itemList[] = $item;
+            }
+        }
+
+        sort($itemList);
+
+        return array_values($itemList);
+    }
+
+    /**
+     * @return list<int>
+     */
+    private function createItemListWithoutSearchedItem(): array
+    {
+        $itemList = [];
+
+        while (count($itemList) < self::ITEM_LIST_SIZE) {
+            $item = mt_rand();
+
+            if ($this->isAddItemToItemList($item, $itemList)) {
+                $itemList[] = $item;
+            }
+        }
+
+        sort($itemList);
+
+        return array_values($itemList);
+    }
+
+    /**
+     * @param list<int> $itemList
+     */
+    private function isAddItemToItemList(int $item, array $itemList): bool
+    {
+        return
+            $item !== self::SEARCHED_ITEM
+                &&
+            !in_array($item, $itemList, true);
+    }
+
+    /**
+     * @return list<int>
+     */
+    private function createItemListForGetInteractionCount(): array
+    {
+        return range(0, self::ITEM_LIST_SIZE_FOR_INTERACTION_COUNT);
+    }
+}
